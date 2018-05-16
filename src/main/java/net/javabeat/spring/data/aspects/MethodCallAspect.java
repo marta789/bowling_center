a2b6b332acb4710bb326be0b07619f105a77d104
Marta789 <marta.ochowicz@gmail.com>
2018-05-16 21:37:17 +0200

    added method counter at ednpoint /statistic

--- src/main/java/net/javabeat/spring/data/aspects/MethodCallAspect.java
+++ src/main/java/net/javabeat/spring/data/aspects/MethodCallAspect.java
@@ -8,28 +8,37 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class MethodCallAspect {
+    public static int getAllReservationsCounter = 0;
+    public static int deleteReservationCounter = 0;
+    public static int updateReservationCounter = 0;
+    public static int addReservationCounter = 0;
+
     @Before("execution(* net.javabeat.spring.data.web.ReservationController.getAllReservations(..))")
     public void beforeAllReservations() {
-        System.out.println("Before get All reservations()");
+        getAllReservationsCounter ++;
+        System.out.println(getAllReservationsCounter);
     }
 
     @Before("execution(* net.javabeat.spring.data.web.ReservationController.getReservation(..))")
     public void beforeGetReservation() {
-        System.out.println("Before get Reservation()");
+        System.out.println("All reservations will be displaied");
     }
 
     @Before("execution(* net.javabeat.spring.data.web.ReservationController.deleteReservation(..))")
     public void beforeDeleteReservation() {
-        System.out.println("Before delete Reservation()");
+        deleteReservationCounter ++;
+        System.out.println("Reservation will to be deleted");
     }
 
     @Before("execution(* net.javabeat.spring.data.web.ReservationController.updateNote(..))")
     public void beforeUpdate() {
-        System.out.println("Before update Reservation()");
+        updateReservationCounter ++;
+        System.out.println("Reservation will be updated");
     }
 
     @Before("execution(* net.javabeat.spring.data.web.ReservationController.createNote(..))")
     public void beforeCreateReservation() {
-        System.out.println("Before create Reservation()");
+        addReservationCounter ++;
+        System.out.println("Reservation will be added");
     }
 }
--- src/main/java/net/javabeat/spring/data/web/ReservationController.java
+++ src/main/java/net/javabeat/spring/data/web/ReservationController.java
@@ -1,10 +1,9 @@
 package net.javabeat.spring.data.web;
-
+import net.javabeat.spring.data.aspects.MethodCallAspect;
 import net.javabeat.spring.data.domain.Reservation;
 import net.javabeat.spring.data.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
 import java.util.List;
 
@@ -75,9 +74,16 @@ public class ReservationController {
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public void deleteReservation(@PathVariable int id) {
-//        Reservation reservation = new Reservation();
-//        reservation.setId(id);
         reservationService.delete(id);
     }
 
+    @RequestMapping(value = "/statistic")
+    public String statistic() {
+        return  "<h1>Method calls statistic</h1>"+
+                "Method getAllReservations() has been called: "+ MethodCallAspect.getAllReservationsCounter+ " times </br>" +
+                "Method addReservation() has been called: "+ MethodCallAspect.addReservationCounter+ " times </br>" +
+                "Method editReservation() has been called: "+ MethodCallAspect.updateReservationCounter+ " times </br>"+
+                "Method deleteReservation() has been called: "+ MethodCallAspect.deleteReservationCounter+ " times </br>";
+
+    }
 }
